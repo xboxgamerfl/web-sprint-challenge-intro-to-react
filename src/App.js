4ad15a068f194fd2882ac095618f2e9b662d68c8
Andrew <dennisandrew09@gmail.com>
2023-06-07 20:41:36 -0400

    challenges done maybe

--- src/App.js
+++ src/App.js
@@ -1,4 +1,46 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
+import axios from 'axios';
+import styled from 'styled-components'
+
+const CharacterWrapper = styled.div`
+  margin-bottom: 20px;
+`;
+
+const Character = ({ characterId }) => {
+  const [character, setCharacter] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://swapi.dev/api/people/${characterId}`);
+        console.log(response.data);
+        setCharacter(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchData();
+  }, [characterId]);
+
+  return (
+    <CharacterWrapper>
+      {character ? <p>{character.name}</p> : <p>Loading...</p>}
+    </CharacterWrapper>
+  );
+};
+
+const CharacterList = () => {
+  const characters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  return (
+    <div>
+      {characters.map(characterId => (
+        <Character key={characterId} characterId={characterId} />
+      ))}
+    </div>
+  );
+};
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,6 +53,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <CharacterList />
     </div>
   );
 }
